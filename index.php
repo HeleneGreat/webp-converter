e87fabc6d841328eebe3f7eefef906e09999ccc8
HeleneGreat <helene.dev@yahoo.com>
2024-07-06 10:40:11 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/style.css">
+    <title>Convertir Image en WebP</title>
+</head>
+
+<body>
+    <section class="container">
+        <h1>Choisir un convertisseur</h1>
+        <a href="./single-picture-converter/" class="btn">Convertir une image</a>
+        <a href="./multiple-pictures-converter/" class="btn">Convertir plusieurs images</a>
+    </section>
+</body>
+
+</html>
